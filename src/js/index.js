e616134c2b384e86e5636007d661c510c2a3d1a4
SergeySKA_10 <karakoskinsergej@gmail.com>
2025-06-16 23:38:28 +0300

    add img

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import { tabs } from './components/tabs/tabs';
-import { technologies } from './components/technologies/technologies';
-import { form } from './components/form/form';
-
-window.addEventListener('DOMContentLoaded', () => {
-    tabs('.portfolio__tabs_li', '.portfolio__pages');
-    technologies('#using', '#learning', '#otherTechnologies');
-    form('#form', '#btn_submit');
-});
