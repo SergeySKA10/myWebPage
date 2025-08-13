64e5b2666dbfe87b3b4eded825844a8705f656b8
SergeySKA_10 <karakoskinsergej@gmail.com>
2025-08-13 17:41:33 +0300

    feat/main - portfolio: update portfolio block

--- src/js/components/portfolio/portfolio.js
+++ src/js/components/portfolio/portfolio.js
@@ -4,7 +4,7 @@ const webProducts = [
         name: 'picture',
         link: 'https://picture.sergeykarakoskin.ru/',
         img: 'https://storage.yandexcloud.net/mywebpageresources/picture.png',
-        stack: 'Реализована frontend часть сайта c использованием JavaScript. (Desktop версия)',
+        stack: 'Реализована frontend часть сайта.<br/><br/> Стек: JavaScript. <br/><br/> Адаптация: Desktop версия',
         category: 'frontend',
     },
     {
@@ -12,7 +12,7 @@ const webProducts = [
         name: 'loan',
         link: 'https://loan.sergeykarakoskin.ru/',
         img: 'https://storage.yandexcloud.net/mywebpageresources/loan.png',
-        stack: 'Реализована frontend часть сайта c использованием JavaScript. (Desktop версия)',
+        stack: 'Реализована frontend часть сайта.<br/><br/> Стек: JavaScript. <br/><br/> Адаптация: Desktop версия',
         category: 'frontend',
     },
     {
@@ -20,7 +20,7 @@ const webProducts = [
         name: 'food',
         link: 'https://test.sergeykarakoskin.ru/',
         img: 'https://storage.yandexcloud.net/mywebpageresources/food.png',
-        stack: 'Реализована frontend часть сайта c использованием JavaScript. (Desktop версия)',
+        stack: 'Реализована frontend часть сайта.<br/><br/> Стек: JavaScript. <br/><br/> Адаптация: Desktop версия',
         category: 'frontend',
     },
     {
@@ -28,7 +28,23 @@ const webProducts = [
         name: 'window',
         link: 'https://window.sergeykarakoskin.ru/',
         img: 'https://storage.yandexcloud.net/mywebpageresources/window.png',
-        stack: 'Реализована frontend часть сайта c использованием JavaScript. (Desktop версия)',
+        stack: 'Реализована frontend часть сайта.<br/><br/> Стек: JavaScript. <br/><br/> Адаптация: Desktop версия',
+        category: 'frontend',
+    },
+    {
+        id: 'todo',
+        name: 'Todo List',
+        link: 'https://mb-pet-todo-list-nyrm.vercel.app/',
+        img: 'https://storage.yandexcloud.net/mywebpageresources/window.png',
+        stack: 'Приложение для формирования списка задач с возможнстью редактирования по статусу выпонения и удаления выполеннных задач из списка.<br/><br/> Стек: TypeScript, React, NextJS.<br/><br/> Адаптация: под различные устройства.<br/><br/> Развертывание: Vercel',
+        category: 'frontend',
+    },
+    {
+        id: 'kinopoisk',
+        name: 'Movie Search',
+        link: 'https://vk-movies-search.vercel.app/',
+        img: 'https://storage.yandexcloud.net/mywebpageresources/window.png',
+        stack: 'Приложение для просмотра данных о фильмах, поиска (с фильтрами) и добавление фильмов в избранное. <br/><br/> Стек: TypeScript, React, NextJS, API Кинопоиск. <br/><br/> Адаптация: под различные устройства.<br/><br/> Развертывание: Vercel',
         category: 'frontend',
     },
 ];
